d2731e975a59992e33c87c50a8f24f47d153cc25
Piecharm <deeprockz93@gmail.com>
2024-08-22 21:22:02 +0530

    header added

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+      <div className="mx-auto max-w-xl text-center p-4">
+          <h1 className="text-3xl font-bold sm:text-4xl">The React Quiz</h1>
+      </div>
+  );
+}
+
+export default Header;
