7f4479c8fcfb96e9fa6d48b2e08ba270079bdc52
Piecharm <deeprockz93@gmail.com>
2024-08-24 23:03:15 +0530

    created fake api using json-server

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
 import Header from "./components/Header";
 
 function App() {
+    useEffect(function () {
+        async function getQuizDetails() {
+            try {
+                const res = await fetch("http://localhost:8000/questions");
+                const data = await res.json();
+                console.log(data);
+            } catch (err) {
+                console.error("Error");
+            }
+        }
+        getQuizDetails();
+    }, []);
+
     return (
         <div>
             <Header />
