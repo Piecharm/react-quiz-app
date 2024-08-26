8738d151ff2abb8715371d6f8151f991d0555100
Piecharm <deeprockz93@gmail.com>
2024-08-26 21:48:04 +0530

    usereducer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./components/Header";
 
+const initialState = {
+    questions: [],
+    status: "loading",
+};
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "GET_QUIZ_SUCCESS":
+            return { ...state, questions: action.payload, status: "ready" };
+        case "GET_QUIZ_FAILURE":
+            return { ...state, status: "error" };
+
+        default:
+            return state;
+    }
+}
+
 function App() {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
     useEffect(function () {
         async function getQuizDetails() {
             try {
                 const res = await fetch("http://localhost:8000/questions");
                 const data = await res.json();
-                console.log(data);
+                dispatch({ type: "GET_QUIZ_SUCCESS", payload: data });
             } catch (err) {
-                console.error("Error");
+                dispatch({ type: "GET_QUIZ_FAILURE" });
             }
         }
         getQuizDetails();
