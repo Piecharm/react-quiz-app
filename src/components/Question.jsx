3141322b1300bea2b8222c30e7e9603b2b1b6e52
Piecharm <deeprockz93@gmail.com>
2024-09-06 23:06:17 +0530

    css added

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,11 +1,11 @@
 function Question({ question }) {
     return (
-        <div>
+        <div className="flex flex-col justify-center items-center w-full">
             <h2 className="font-semibold">{question.question}</h2>
-            <div>
+            <div className="flex flex-col py-2">
                 {question.options.map((option) => (
                     <button
-                        className="bg-[#fff9de] text-gray-800 hover:bg-gray-900 hover:text-white font-semibold py-2 px-4 border border-gray-400 rounded"
+                        className="bg-[#fff9de] text-gray-800 hover:bg-gray-900 hover:text-white font-semibold py-2 px-4 border border-gray-400 rounded my-3"
                         key={option}
                     >
                         {option}
